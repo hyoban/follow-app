51b7d6f17fca3d3561cd950e55d20354da5651f8
Stephen Zhou <hi@hyoban.cc>
2024-07-27 11:51:44 +0800

    chore: enable no-explicit-any

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
 import { defineConfig } from 'eslint-config-hyoban'
 
-export default defineConfig({
-  ignores: ['api/hono.ts', 'drizzle'],
-  typeChecked: 'essential',
-  project: true,
-  tsconfigRootDir: import.meta.dirname,
-})
+export default defineConfig(
+  {
+    ignores: ['api/hono.ts', 'drizzle'],
+    typeChecked: 'essential',
+    project: true,
+    tsconfigRootDir: import.meta.dirname,
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+)
