2a56fbe5e052289ab5138c2edfbc4a6d8b9f64ea
enpitsulin <enpitsulin@gmail.com>
2024-08-16 17:07:30 +0800

    feat: add footer bar to entryId

--- atom/entry-list.ts
+++ atom/entry-list.ts
@@ -1,3 +1,6 @@
+import { atom } from 'jotai'
+
 import { atomWithStorage } from './storage'
 
 export const showUnreadOnlyAtom = atomWithStorage<boolean>('unread-only-list', false)
+export const showFooterAtom = atom<boolean>(true)
--- consts/limit.ts
+++ consts/limit.ts
@@ -1,1 +1,2 @@
 export const FETCH_PAGE_SIZE = 20
+export const READ_USER_AVATAR_COUNT = 6
