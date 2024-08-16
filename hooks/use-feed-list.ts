ff18b58722273143c749b23938fd6814a7d91c59
Stephen Zhou <hi@hyoban.cc>
2024-08-16 21:04:05 +0800

    fix: showUnreadOnly filter feed

--- hooks/use-feed-list.ts
+++ hooks/use-feed-list.ts
@@ -1,15 +1,23 @@
+import { useAtomValue } from 'jotai'
+
+import { showUnreadOnlyAtom } from '~/atom/entry-list'
 import type { TabViewIndex } from '~/atom/layout'
 import { db } from '~/db'
 
 import { useQuerySubscription } from './use-query-subscription'
 
 export function useFeedList(view: TabViewIndex) {
+  const showUnreadOnly = useAtomValue(showUnreadOnlyAtom)
   return useQuerySubscription(
     db.query.feeds.findMany({
-      where(schema, { eq }) {
+      where(schema, { eq, and, not }) {
+        if (showUnreadOnly) {
+          return and(eq(schema.view, view), not(eq(schema.unread, 0)))
+        }
+
         return eq(schema.view, view)
       },
     }),
-    ['feeds', { view }],
+    ['feeds', { view, showUnreadOnly }],
   )
 }
