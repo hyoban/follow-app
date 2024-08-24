4cc79c3cb225b7c233fedf88f39be4852da5f393
Stephen Zhou <hi@hyoban.cc>
2024-08-24 12:29:56 +0800

    chore: make expo dom build work
    export EAS_LOCAL_BUILD_SKIP_CLEANUP=1 to check logs
    https://github.com/facebook/metro/issues/330

--- metro.config.js
+++ metro.config.js
@@ -8,85 +8,17 @@ config.resolver.sourceExts.push('sql')
 
 // https://github.com/expo/expo/issues/22482
 config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (platform === 'web') {
-    if (
-      moduleName.endsWith('./Platform')
-      || moduleName.endsWith('../Utilities/Platform')
-      || moduleName.endsWith('../Libraries/Utilities/Platform')
-    ) {
-      return {
-        filePath: require.resolve('react-native-web/dist/exports/Platform'),
-        type: 'sourceFile',
-      }
-    }
+  // * .expo/@dom/00bb935b368d3519cca2a1dc067f71ec1f66887b.js(.web.ts|.ts|.web.tsx|.tsx|.web.mjs|.mjs|.web.js|.js|.web.jsx|.jsx|.web.json|.json|.web.cjs|.cjs|.web.scss|.scss|.web.sass|.sass|.web.css|.css|.web.sql|.sql)
+  // * .expo/@dom/00bb935b368d3519cca2a1dc067f71ec1f66887b.js/index(.web.ts|.ts|.web.tsx|.tsx|.web.mjs|.mjs|.web.js|.js|.web.jsx|.jsx|.web.json|.json|.web.cjs|.cjs|.web.scss|.scss|.web.sass|.sass|.web.css|.css|.web.sql|.sql)
 
-    if (moduleName.endsWith('../Components/AccessibilityInfo/legacySendAccessibilityEvent')) {
-      return {
-        filePath: require.resolve('react-native-web/dist/exports/AccessibilityInfo'),
-        type: 'sourceFile',
-      }
-    }
+  // For expo dom, we need to directly resolve the source files
 
-    if (moduleName.endsWith('./PlatformColorValueTypes')) {
-      return {
-        filePath: require.resolve('react-native-web/dist/exports/StyleSheet'),
-        type: 'sourceFile',
-      }
+  if (moduleName.includes('.expo/@dom/')) {
+    return {
+      filePath: moduleName,
+      type: 'sourceFile',
     }
-
-    if (moduleName.endsWith('RCTNetworking')) {
-      return {
-        filePath: require.resolve('identity-obj-proxy'),
-        type: 'sourceFile',
-      }
-    }
-
-    if (moduleName.endsWith('./RCTAlertManager')) {
-      return {
-        filePath: require.resolve('identity-obj-proxy'),
-        type: 'sourceFile',
-      }
-    }
-
-    if (moduleName.endsWith('DevToolsSettings/DevToolsSettingsManager')) {
-      return {
-        filePath: require.resolve('identity-obj-proxy'),
-        type: 'sourceFile',
-      }
-    }
-
-    if (moduleName.endsWith('Utilities/BackHandler')) {
-      return {
-        filePath: require.resolve('react-native-web/dist/exports/BackHandler'),
-        type: 'sourceFile',
-      }
-    }
-
-    if (moduleName.endsWith('BaseViewConfig')) {
-      return {
-        filePath: require.resolve('identity-obj-proxy'),
-        type: 'sourceFile',
-      }
-    }
-
-    if (moduleName.endsWith('../Image/Image')) {
-      return {
-        filePath: require.resolve('identity-obj-proxy'),
-        type: 'sourceFile',
-      }
-    }
-
-    if (moduleName.endsWith('./StyleSheet/PlatformColorValueTypes')) {
-      return {
-        filePath: require.resolve('identity-obj-proxy'),
-        type: 'sourceFile',
-      }
-    }
-
-    // Logic to resolve the module name to a file path...
-    // NOTE: Throw an error if there is no resolution.
   }
-  // Optionally, chain to the standard Metro resolver.
   return context.resolveRequest(context, moduleName, platform)
 }
 
