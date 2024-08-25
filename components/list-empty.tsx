ae7d6287c3f7a8aef2ad5dd77b4d80166ee5cb6f
Stephen Zhou <hi@hyoban.cc>
2024-08-25 21:00:45 +0800

    fix: delay empty view

--- components/list-empty.tsx
+++ components/list-empty.tsx
@@ -1,17 +1,39 @@
+import { useEffect, useState } from 'react'
+
 import { Column } from './flex'
 import { Iconify } from './icon'
 import { Text } from './text'
 
+type Props = {
+  children: React.ReactNode
+  waitBeforeShow?: number
+}
+
+function Delayed({ children, waitBeforeShow = 200 }: Props) {
+  const [isShown, setIsShown] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsShown(true)
+    }, waitBeforeShow)
+    return () => clearTimeout(timer)
+  }, [waitBeforeShow])
+
+  return isShown ? children : null
+}
+
 export function ListEmpty() {
   return (
-    <Column
-      h={150}
-      align="center"
-      justify="center"
-      gap={10}
-    >
-      <Iconify icon="mgc:celebrate-cute-re" size={50} />
-      <Text size={20}>Nothing here</Text>
-    </Column>
+    <Delayed>
+      <Column
+        h={150}
+        align="center"
+        justify="center"
+        gap={10}
+      >
+        <Iconify icon="mgc:celebrate-cute-re" size={50} />
+        <Text size={20}>Nothing here</Text>
+      </Column>
+    </Delayed>
   )
 }
