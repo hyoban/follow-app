e29c61948a120b3deced705ede5e527467c00671
Stephen Zhou <hi@hyoban.cc>
2024-08-25 23:56:38 +0800

    fix: bg color

--- components/drawer-content.tsx
+++ components/drawer-content.tsx
@@ -2,6 +2,7 @@ import type { DrawerContentComponentProps } from '@react-navigation/drawer'
 import { useRouter } from 'expo-router'
 import { Pressable, ScrollView } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { useStyles } from 'react-native-unistyles'
 
 import type { TabViewIndex } from '~/store/layout'
 
@@ -13,9 +14,16 @@ export function DrawerContent({ state }: DrawerContentComponentProps) {
   const tabRoutes = state.routes[state.index]?.state?.routes
   const currentTabIndex = tabRoutes?.[0]?.state?.index ?? 0
   const router = useRouter()
+  const { theme } = useStyles()
 
   return (
-    <SafeAreaView style={{ justifyContent: 'space-between', flex: 1 }}>
+    <SafeAreaView
+      style={{
+        justifyContent: 'space-between',
+        flex: 1,
+        backgroundColor: theme.colors.gray1,
+      }}
+    >
       <ScrollView>
         <FeedList view={currentTabIndex as TabViewIndex} inDrawer />
       </ScrollView>
