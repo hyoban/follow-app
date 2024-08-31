9b95a69bc101692275cdf58807cfd087a9506756
Stephen Zhou <hi@hyoban.cc>
2024-08-31 20:35:46 +0800

    fix: do not hide empty unread feed

--- hooks/use-feed-list.ts
+++ hooks/use-feed-list.ts
@@ -1,15 +1,9 @@
-import { atom, getDefaultStore, useAtom, useAtomValue } from 'jotai'
-
 import { db } from '~/db'
-import { showUnreadOnlyAtom } from '~/store/entry'
 import type { TabViewIndex } from '~/store/layout'
 
 import { useQuerySubscription } from './use-query-subscription'
 
-const unreadFeedAtom = atom<Set<string> | null>(null)
-
 export function useFeedList(view: TabViewIndex) {
-  const showUnreadOnly = useAtomValue(showUnreadOnlyAtom)
   const sub = useQuerySubscription(
     db.query.feeds.findMany({
       where(schema, { eq }) {
@@ -17,23 +11,7 @@ export function useFeedList(view: TabViewIndex) {
       },
     }),
     ['feeds', { view }],
-    {
-      afterRevalidate(data) {
-        const showUnreadOnly = getDefaultStore().get(showUnreadOnlyAtom)
-        if (!showUnreadOnly && data) {
-          setUnreadItems(new Set(data.filter(i => i.unread).map(i => i.id)))
-        }
-      },
-    },
   )
 
-  const [unreadItems, setUnreadItems] = useAtom(unreadFeedAtom)
-  if (!unreadItems && sub.data) {
-    setUnreadItems(new Set(sub.data.filter(i => i.unread).map(i => i.id)))
-  }
-
-  return {
-    ...sub,
-    data: sub.data?.filter(i => !showUnreadOnly || !unreadItems || unreadItems.has(i.id)),
-  }
+  return sub
 }
